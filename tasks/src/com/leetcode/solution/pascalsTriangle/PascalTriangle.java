d4e86817b12f07d8f8b4397cd1fe007229144e1d
Vitaliy Lyashchenko <69591510+Joroboro253@users.noreply.github.com>
2023-09-11 00:03:19 +0300

    Some resolving of first task

---
+++ tasks/src/com/leetcode/solution/pascalsTriangle/PascalTriangle.java
@@ -0,0 +1,14 @@
+package com.leetcode.solution.pascalsTriangle;
+
+import java.util.ArrayList;
+import java.util.List;
+// too early
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+
+
+        return triangle;
+    }
+}
