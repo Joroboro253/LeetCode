1752f511c99b5c57aafc83b9c4ff1b84769f2db5
Vitaliy Lyashchenko <69591510+Joroboro253@users.noreply.github.com>
2023-09-20 18:17:48 +0300

    First task for git

---
+++ tasks/src/Main.java
@@ -0,0 +1,14 @@
+import com.leetcode.solution.pascalsTriangle.PascalTriangle;
+import com.leetcode.solution.removeDuplicatesFromSortedArray.Solution;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {0,0,0,0,0};
+
+        System.out.println(solution.removeDuplicates(nums));
+
+    }
+}
